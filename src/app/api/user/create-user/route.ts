1b0dff3cc57b69b509681af93aec2703663ed0f5
Uday Hasan <udayhasan15@gmail.com>
2024-08-26 03:31:36 +0600

    Error:add user to db. Sol: Connect to db

--- src/app/api/user/create-user/route.ts
+++ src/app/api/user/create-user/route.ts
@@ -1,3 +1,4 @@
+import { connectToDB } from "@/lib/database/connectToDB";
 import { User } from "@/lib/database/db_model/user.models";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
@@ -8,6 +9,7 @@ export const POST = async (req: Request, res: Response) => {
     if (!userId) {
       return NextResponse.json({ message: "Unauthorized", success: false });
     }
+    await connectToDB();
     const { name, email } = await req.json();
     const userData = { name, email, clerkId: userId };
     const newUser = await User.create(userData);
